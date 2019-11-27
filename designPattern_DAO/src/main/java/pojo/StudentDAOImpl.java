7cda1d51ef5a521a7932b8060bb84ff7862dd15c
irfan <irfan.naikwade@gmail.com>
2019-11-27 07:15:07 +0530

    doa

--- designPattern_DAO/src/main/java/pojo/StudentDAOImpl.java
+++ designPattern_DAO/src/main/java/pojo/StudentDAOImpl.java
@@ -1,15 +1,24 @@
 package pojo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StudentDAOImpl implements StudentDAO{
 
-	List<Student> students;
+//	List<Student> students;
+	
+//	direct adding 1 where you cannpt modify it
+	List<Student> students=Arrays.asList( new Student(1,"N1","C1"),new Student(2,"N2","C3"));
+//	students.add   // cannot add
+	
+	
 	public StudentDAOImpl() {
 		students = new ArrayList<Student>();
 		Student student1= new Student(1,"Robert","Nashivlle");
 		students.add(student1);
+		
+//		direct adding
 		students.add(new Student(2,"Irfan","Pune"));
 	}
 
